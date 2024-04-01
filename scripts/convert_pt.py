694a9532b3534f5e06ba5ce99e5e6d122b997bf3
Tianhua Tao <taotianhua@outlook.com>
2024-03-31 22:00:26 -0500

    add

---
+++ scripts/convert_pt.py
@@ -0,0 +1,6 @@
+import torch
+
+
+data = torch.load('/mnt/ssd1/agpt/agpt-dpo/direct-preference-optimization/.cache/tth/anthropic_dpo_pythia28_2024-03-31_02-46-49_492693/step-512/policy.pt')
+
+print(data)
